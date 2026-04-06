d7dc2c02fcc0cea6a0afc417652be48309ee59cb
KittyMew0 <90781550+KittyMew0@users.noreply.github.com>
2026-04-06 17:36:32 +0300

    Update Main.java

--- lesson6/src/main/java/org/example/Main.java
+++ lesson6/src/main/java/org/example/Main.java
@@ -7,10 +7,6 @@ import org.example.notes.presentation.queries.controllers. NotesController;
 import org.example.notes.presentation.queries.views.NotesConsolePresenter;
 
 public class Main {
-
-    // в дз хочет видеть какую то схему...
-    // или добавить че то, он не решил (ну там в мейн показать что
-    // заметка добавляется, удаляется, бла бла бла...
     public static void main(String[] args) {
         NotesController controller = new NotesController(
                 new ConcreteNoteEditor(new NotesDbContext(new NotesDatabase()), new NotesConsolePresenter()));
