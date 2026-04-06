7b2012e75acb561a3afc45781eb87bbfab95a45a
KittyMew0 <90781550+KittyMew0@users.noreply.github.com>
2026-04-06 17:34:56 +0300

    Update Main.java

--- lesson4/src/main/java/org/example/Main.java
+++ lesson4/src/main/java/org/example/Main.java
@@ -1,11 +1,6 @@
 package org.example;
 
 public class Main {
-
-    // дз: сделать так чтобы это работало.
-    // понятия уже не имею, доделано ли - вряд ли формально,
-    // но я не вижу сообщений об ошибках.
-    // да вряд ли, я ж ничего еще не делала
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
